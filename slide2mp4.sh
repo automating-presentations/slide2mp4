8a0f7e9620b468adca56af9adc97d7f891f5b680
Hirofumi Kojima <hkojima@redhat.com>
2021-09-07 14:42:38 +0900

    updated

--- slide2mp4.sh
+++ slide2mp4.sh
@@ -73,7 +73,7 @@ print_usage ()
 	echo "Example6: The following command specifies the use of Azure Speech. The subscription key to use Azure Speech must be found in \"~/azure/.tts-subs-keyfile\". When you run this command, \"test-lexicon.pls\" will be temporarily uploaded to Amazon S3."
 	echo "	$(basename $0) -azure test-slides.pdf test-slides.txt test-lexicon.pls test-output.mp4"
 	echo ""
-	echo "Example7: The following command specifies the Azure Region, voice name/pitch, subscription keyfile path to use Azure Speech. When using Azure Speech, you can specify public (non-private) URL where you can refer to \"test.pls\"."
+	echo "Example7: The following command specifies the Azure Region, voice name/pitch, subscription keyfile path to use Azure Speech. When using Azure Speech, you can specify public (non-private) URL where you can refer to \"test.pls\". If you specify public URL, Amazon S3 is not used in slide2mp4."
 	echo "	$(basename $0) -azure -azure-region centralus -azure-vid en-US-JennyNeural -azure-pitch -6 -azure-tts-key test-azure-keyfile test.pdf test.txt http://publicdomain/test.pls output.mp4"
 	exit
 }
